941b8c46cccb043504f3c6993c10d95e72cc8869
wumao <99360774@qq.com>
2021-05-20 21:01:51 +0800

    86

--- 20.有效的括号.js
+++ 20.有效的括号.js
@@ -10,13 +10,14 @@ var isValid = function (s) {
   const stack = []
   for (let i = 0; i < s.length; i++) {
     const element = s[i]
-    if (map.has(element)) {
-      stack.push(map.get(element))
+    const matchVal = map.get(element)
+    if (matchVal) {
+      stack.push(matchVal)
     } else {
       if (stack.pop() !== element) return false
     }
   }
-  return !stack.length
+  return stack.length === 0
 };
 
 console.log(isValid("()"));
