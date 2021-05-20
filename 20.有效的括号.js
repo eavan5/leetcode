50c5227ac4796874d22d6fe1e230ee2dc27f9ca4
wumao <99360774@qq.com>
2021-05-20 20:58:42 +0800

    feat:有效括号

--- 20.有效的括号.js
+++ 20.有效的括号.js
@@ -1,44 +1,22 @@
-/*
- * @lc app=leetcode.cn id=20 lang=javascript
- *
- * [20] 有效的括号
- */
-
-// @lc code=start
 /**
  * @param {string} s
  * @return {boolean}
  */
 var isValid = function (s) {
-  if (s.length % 2) return false
-  let stack = []
   const map = new Map()
-  map.set(')', '(')
-  map.set(']', '[')
-  map.set('}', '{')
-  // for (let index = 0, length = s.length; index < length; index++) {
-  //   const i = s[index];
-  //   if (["(", "[", "{"].includes(i)) {
-  //     stack.push(i)
-  //   } else {
-  //     if (stack.pop() !== map.get(i)) {
-  //       return false
-  //     }
-  //   }
-  // }
-  for (const i of s) {
-    if (["(", "[", "{"].includes(i)) {
-      stack.push(i)
+  map.set('(', ')')
+  map.set('{', '}')
+  map.set('[', ']')
+  const stack = []
+  for (let i = 0; i < s.length; i++) {
+    const element = s[i]
+    if (map.has(element)) {
+      stack.push(map.get(element))
     } else {
-      if (stack.pop() !== map.get(i)) {
-        return false
-      }
+      if (stack.pop() !== element) return false
     }
   }
-  // if (!stack.length) return true
-  // else return false
   return !stack.length
 };
-// console.log(isValid("()"));
-// @lc code=end
 
+console.log(isValid("()"));
