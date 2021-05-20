90caf186f48eb2a18cdaa1672012ed6735402621
wumao <99360774@qq.com>
2021-05-20 15:48:31 +0800

    feat:斐波那契额动态规划

--- 509.斐波那契数.js
+++ 509.斐波那契数.js
@@ -16,5 +16,29 @@ var fib = function (n) {
   }
   return array[n]
 };
+
+var fib = function (n) {
+  const array = [0, 1]
+  for (let index = 2; index <= 30; index++) {
+    array[index] = array[index - 1] + array[index - 2]
+  }
+  return array[n]
+};
+
+// 动态规划
+var fib = function (n) {
+  const array = []
+  array.length = n + 1
+  array.fill(0)
+  array[1] = 1
+  array[2] = 1
+  let i
+  for (i = 3; i <= n; i++) {
+    array[i] = array[i - 1] + array[i - 2]
+  }
+  return array[n]
+};
+
+console.log(fib(0));
 // @lc code=end
 
