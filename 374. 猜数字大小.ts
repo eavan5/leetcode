d4d652153488c0e43298a31cab85d4548738ff0c
wumao <99360774@qq.com>
2021-09-02 23:37:51 +0800

    直接二分 加细节

--- 374. 猜数字大小.ts
+++ 374. 猜数字大小.ts
@@ -7,18 +7,33 @@
  * var guess = function(num) {}
  */
 
+// 递归+二分
+// function guessNumber(n: number): number {
+//   const recursion = (min: number, max: number) => {
+//     const mid = Math.floor((min + max) / 2)
+//     const res = guess(mid)
+//     if (res === -1) {
+//       return recursion(1,mid-1)
+//     } else if(res === 1){
+//       return recursion(mid+1,max)
+//     }else {
+//       return mid
+//     }
+//   }
+//   return recursion(1,n)
+// };
+
+// 二分
 
 function guessNumber(n: number): number {
-  const recursion = (min: number, max: number) => {
-    const mid = Math.floor((min + max) / 2)
-    const res = guess(mid)
-    if (res === -1) {
-      return recursion(1,mid-1)
-    } else if(res === 1){
-      return recursion(mid+1,max)
-    }else {
-      return mid
+  let left = 1, right = n
+  while (left < right) {
+    const mid = Math.floor(left+(right - left) / 2) //不要使用(left+right )/2  因为加起来可能会超过限制
+    if (guess(mid) <= 0) {
+      right = mid
+    } else {
+      left = mid+1
     }
   }
-  return recursion(1,n)
-};
+  return right
+}
