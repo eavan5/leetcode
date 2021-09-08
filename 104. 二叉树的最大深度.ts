127facddb26ee451bd7e9a8809880f9ce4268995
wumao <99360774@qq.com>
2021-09-08 22:11:27 +0800

    feat:递归 暂定

---
+++ 104. 二叉树的最大深度.ts
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+import { TreeNode } from "./types"
+
+function maxDepth(root: TreeNode | null): number {
+  if(!root) return 0
+  let res = 1
+  function deep(target: TreeNode | null, count: number) {
+    if (!target) return
+    deep(target.left, count + 1)
+    deep(target.right, count + 1)
+    res = Math.max(res, count)
+  }
+  deep(root, res)
+  return res
+};
---
+++ types.d.ts
@@ -0,0 +1,19 @@
+export class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+  }
+}
+
+declare class TreeNode {
+  val: number
+  left: TreeNode | null
+  right: TreeNode | null
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+  }
+}
