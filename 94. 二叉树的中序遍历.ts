0dccebe018d527e696806f55d37ea2ea29f6df31
wumao <99360774@qq.com>
2021-09-25 20:49:54 +0800

    +1

---
+++ 94. 二叉树的中序遍历.ts
@@ -0,0 +1,22 @@
+import { TreeNode } from './types'
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function inorderTraversal(root: TreeNode | null, temp: number[] = []): number[] {
+  if (!root) return temp
+  inorderTraversal(root.left,temp)
+  temp.push(root.val)
+  inorderTraversal(root.right,temp)
+  return temp
+};
