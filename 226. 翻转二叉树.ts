588999aff3f8cc3378cdf42d2ec4f79158a6df95
wumao <99360774@qq.com>
2021-09-30 22:24:11 +0800

    +1

---
+++ 226. 翻转二叉树.ts
@@ -0,0 +1,22 @@
+import { TreeNode } from "./types";
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function invertTree(root: TreeNode | null): TreeNode | null {
+  if (!root) return null;
+  invertTree(root.left);
+  invertTree(root.right);
+  [root.left, root.right] = [root.right, root.left];
+  return root;
+}
