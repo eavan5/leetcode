e967a03579e8271f40cfb4ff6d26b3ef0af3a548
wumao <99360774@qq.com>
2021-10-27 21:44:58 +0800

    +1

--- 509.斐波那契数.ts
+++ 509.斐波那契数.ts
@@ -1,12 +1,10 @@
-const dp = [0, 1]
-const map = new Map()
-
 function fib(n: number): number {
-  if (n < 2) return dp[n]
-  if (map.has(n)) return map.get(n)
-  const res = fib(n - 1) + fib(n - 2)
-  map.set(n, res)
-  return res
+  if(n<=1) return n
+  let l = 0, m = 0, r = 1
+  for (let j = 2; j <= n; j++){
+    l = m 
+    m = r
+    r = l+m
+  }
+  return r
 };
-
-console.log(fib(4));
