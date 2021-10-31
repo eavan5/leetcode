e6ff411f09110aa62a247dffe99c390e367db35e
wumao <99360774@qq.com>
2021-10-31 17:30:00 +0800

    0

--- 213. 打家劫舍 II.ts
+++ 213. 打家劫舍 II.ts
@@ -1,9 +1,17 @@
 function rob(nums: number[]): number {
+
   if (!nums.length) {
     return 0
   } else if (nums.length === 1) {
     return nums[0]
   }
+/*   这时候因为沿街的房屋都是拼接在一起的，意思就是说你偷了第一家就不能偷最后一家
+
+实际上这个和打家劫舍第一题是一模一样的，不过就是多了一层
+
+如果偷第一家，那就偷不了最后一家，那被偷的范围就是dp[0,i-2]
+
+如果偷了最后一家，那就偷不了第一家，被偷的范围就是dp[1,i-1] */
   return Math.max(reckon(nums, 0, nums.length - 2), reckon(nums, 1, nums.length - 1))
 
 };
