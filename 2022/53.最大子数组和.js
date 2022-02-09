0a741afb032c5de8617b43331f63ba652978dba5
wumao <webwwy@gmail.com>
2022-02-09 10:07:29 +0800

    +1

---
+++ 2022/53.最大子数组和.js
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=53 lang=javascript
+ *
+ * [53] 最大子数组和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+  const dp = [nums[0]]
+  let max = dp[0]
+  for (let i = 1; i < nums.length; i++) {
+    dp[i] = Math.max(dp[i - 1] + nums[i], nums[i])
+    max = Math.max(dp[i], max)
+  }
+  return max
+};
+// @lc code=end
+
