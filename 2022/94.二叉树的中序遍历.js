8c21d7bb64c544625f494b15affe520c67c00073
wumao <webwwy@gmail.com>
2022-02-17 00:59:02 +0800

    +1

---
+++ 2022/94.二叉树的中序遍历.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=javascript
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root, temp = []) {
+  if (!root) return temp
+  inorderTraversal(root.left, temp)
+  temp.push(root.val)
+  inorderTraversal(root.right, temp)
+  return temp
+};
+// @lc code=end
+
