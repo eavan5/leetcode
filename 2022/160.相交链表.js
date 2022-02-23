2516998b1a556511d92c534a917d233ef1159ff5
wumao <webwwy@gmail.com>
2022-02-23 22:16:23 +0800

    +1

---
+++ 2022/160.相交链表.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=160 lang=javascript
+ *
+ * [160] 相交链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function (headA, headB) {
+  let left = headA, right = headB
+  while (left !== right) {
+    left = !left ? headB : left.next
+    right = !right ? headA : right.next
+  }
+  return left
+};
+// @lc code=end
+
