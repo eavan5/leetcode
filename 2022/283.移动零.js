52e2a75ddb51c86cf11bd59442bec4c57cefbbdb
wumao <webwwy@gmail.com>
2022-03-01 00:32:48 +0800

    +1

--- 2022/283.移动零.js
+++ 2022/283.移动零.js
@@ -15,3 +15,18 @@ var moveZeroes = function (nums) {
 };
 
 // 这个效率不高,所以要用快慢指针
+
+
+function moveZeroes (nums) {
+  let s = 0
+  for (let f = 0; f < nums.length; f++) {
+    if (nums[f]) {
+      nums[s++] = nums[f]
+    }
+  }
+  //   for (; s < nums.length; s++) {
+  //     nums[s] = 0
+  //   }
+  const count = nums.length - s
+  nums.splice(s, count, ...new Array(count).fill(0))
+};
