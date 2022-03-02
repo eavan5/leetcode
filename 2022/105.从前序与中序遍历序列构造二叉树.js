49733de502640d87e47fd79258690d33bd3b082f
wumao <webwwy@gmail.com>
2022-03-02 15:07:56 +0800

    fix

--- 2022/105.从前序与中序遍历序列构造二叉树.js
+++ 2022/105.从前序与中序遍历序列构造二叉树.js
@@ -19,7 +19,7 @@
  * @return {TreeNode}
  */
 var buildTree = function (preorder, inorder) {
-  if (!root) return null
+  if (!preorder.length) return null
   const root = new TreeNode(preorder[0])
   const mid = inorder.indexOf(preorder[0])
   root.left = buildTree(preorder.slice(1, mid + 1), inorder.slice(0, mid))
