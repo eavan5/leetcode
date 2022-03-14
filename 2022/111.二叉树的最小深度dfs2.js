2942fa3d72aa2b1a956f305d8beab0a4f96b870d
wumao <webwwy@gmail.com>
2022-03-14 21:39:47 +0800

    +1

--- 2022/111.二叉树的最小深度dfs2.js
+++ 2022/111.二叉树的最小深度dfs2.js
@@ -19,16 +19,10 @@
  */
 var minDepth = function (root) {
   if (!root) return 0
-  let min = Number.MAX_SAFE_INTEGER
-  dfs(root)
-  return min
-  function dfs (root, deep = 1) {
-    if (!root.left && !root.right) {
-      return min = Math.min(min, deep)
-    }
-    root?.left && dfs(root.left, deep + 1)
-    root?.right && dfs(root.right, deep + 1)
-  }
+  if (!root.left && !root.right) return 1
+  if (!root.left) return 1 + minDepth(root.right)
+  if (!root.right) return 1 + minDepth(root.left)
+  return Math.min(minDepth(root.left), minDepth(root.right)) + 1
 };
 // @lc code=end
 
