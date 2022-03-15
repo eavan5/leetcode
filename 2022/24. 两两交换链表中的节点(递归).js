3302d27ace095c783efe082e43268109c0b9f1cd
wumao <webwwy@gmail.com>
2022-03-15 15:31:05 +0800

    +1

---
+++ 2022/24. 两两交换链表中的节点(递归).js
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function (head) {
+  if (head == null || head.next == null) {
+    return head;
+  }
+  const next = head.next
+  head.next = swapPairs(head.next.next)
+  next.next = head
+  return next
+};
