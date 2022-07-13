0c497573108677258fb9ad01a4e079aa5084e5cd
wumao <webwwy@gmail.com>
2022-07-13 15:02:52 +0800

    +1

---
+++ 2022/55.跳跃游戏(贪心).js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=55 lang=javascript
+ *
+ * [55] 跳跃游戏
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function (nums) {
+	if (nums.length === 1) return true
+	let cover = 0
+	for (let i = 0; i <= cover; i++) {
+		cover = Math.max(nums[i] + i, cover)
+		if (cover >= nums.length - 1) return true
+	}
+	return false
+}
+// @lc code=end
