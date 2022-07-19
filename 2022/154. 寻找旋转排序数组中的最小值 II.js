e7d12b2116c3d4af0b9385eb88eddf5e19f02178
wumao <webwwy@gmail.com>
2022-07-20 00:25:38 +0800

    +1

---
+++ 2022/154. 寻找旋转排序数组中的最小值 II.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function (nums) {
+	let [l, r] = [0, nums.length - 1]
+	while (l < r) {
+		const mid = (l + r) >> 1
+		if (nums[mid] == nums[r]) {
+			r--
+		} else if (nums[mid] > nums[r]) {
+			l = mid + 1
+		} else {
+			r = mid
+		}
+	}
+	return nums[l]
+}
