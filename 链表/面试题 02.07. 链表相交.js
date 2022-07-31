5d99d756fa5dd90357707d75f841ea73480568f5
wumao <webwwy@gmail.com>
2022-07-31 17:04:26 +0800

    +1

---
+++ 链表/面试题 02.07. 链表相交.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function (headA, headB) {
+	let a = headA,
+		b = headB
+	while (a !== b) {
+		a = a ? a.next : headA
+		b = b ? b.next : headB
+	}
+	return a
+}
