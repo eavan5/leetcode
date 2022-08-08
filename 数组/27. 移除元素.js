859826d7aea433c2e117b24c8b5fa8ec0996762c
wumao <webwwy@gmail.com>
2022-08-08 22:38:43 +0800

    +1

---
+++ 数组/27. 移除元素.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+	let arrow = 0
+	for (let i = 0; i < nums.length; i++) {
+		if (nums[i] !== val) {
+			nums[arrow++] = nums[i]
+		}
+	}
+	return arrow
+}
