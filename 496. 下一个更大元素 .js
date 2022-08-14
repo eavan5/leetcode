fdc739fbc9a8ebf48ccc4620ab28a23697790a21
wumao <webwwy@gmail.com>
2022-08-14 20:52:18 +0800

    1

--- 496. 下一个更大元素 .js
+++ 496. 下一个更大元素 .js
@@ -4,26 +4,17 @@
  * @return {number[]}
  */
 var nextGreaterElement = function (nums1, nums2) {
-	const map = new Array(nums2.length).fill(-1)
-	const stack = [],
-		res = new Array(nums1.length).fill(-1)
+	const stack = []
+	const map = new Map()
 	for (let i = 0; i < nums2.length; i++) {
-		const current = nums2[i]
-		while (stack.length && nums2[stack.at(-1)] < current) {
-			map[stack.pop()] = current
+		while (stack.length && nums2[i] > nums2[stack.at(-1)]) {
+			map.set(nums2[stack.pop()], nums2[i])
 		}
 		stack.push(i)
 	}
-
+	const res = []
 	for (let i = 0; i < nums1.length; i++) {
-		const index = nums2.indexOf(nums1[i])
-		res[i] = map[index]
+		res.push(map.get(nums1[i]) || -1)
 	}
-
 	return res
 }
-
-console.log(nextGreaterElement([4, 1, 2], [1, 3, 4, 2]))
-
-// stack 2
-// map  [1,2,0,0]
