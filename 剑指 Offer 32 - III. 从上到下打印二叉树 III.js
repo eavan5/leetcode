f64e2f541bbfe91e83e057eede2860e8b192ffe7
wumao <webwwy@gmail.com>
2022-09-05 23:09:50 +0800

    +1

---
+++ 剑指 Offer 32 - III. 从上到下打印二叉树 III.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+  if(!root) return []
+  const queue = [root]
+  const res = []
+  let flag = true
+  while(queue.length){
+    const size = queue.length
+    const temp = []
+    for(let i = 0;i<size;i++){
+      const current = queue.shift()
+      temp.push(current.val)
+      current.left && queue.push(current.left)
+      current.right && queue.push(current.right)
+    }
+    if(!flag){
+      temp.reverse()
+    }
+    flag = !flag
+    res.push(temp)
+  }
+  return res
+};
