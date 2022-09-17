7213fab38aefc526f15af3626af29584c7774dfd
wumao <webwwy@gmail.com>
2022-09-17 21:08:58 +0800

    1

--- 剑指 Offer 21. 调整数组顺序使奇数位于偶数前面.js
+++ 剑指 Offer 21. 调整数组顺序使奇数位于偶数前面.js
@@ -6,12 +6,9 @@ var exchange = function (nums) {
 	let l = 0,
 		r = nums.length - 1
 	while (l < r) {
-		if (nums[l] % 2 === 0) {
-			;[nums[l], nums[r]] = [nums[r], nums[l]]
-			r--
-		} else {
-			l++
-		}
+		while (l < r && (nums[l] & 1) === 1) l++
+		while (l < r && (nums[r] & 1) === 0) r--
+		;[nums[l], nums[r]] = [nums[r], nums[l]]
 	}
 	return nums
 }
