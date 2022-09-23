cf2946e0e78bfea4d9bcfcffc8164f92f7ae435f
Eavan <webwwy@gmail.com>
2022-09-23 22:48:10 +0800

    +1

--- 88. 合并两个有序数组.js
+++ 88. 合并两个有序数组.js
@@ -6,21 +6,16 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function (nums1, m, nums2, n) {
-  let i = 0, j = 0
-  const map = Array.from({ length: m + n }).fill(0)
-  while (i < m || j < n) {
-    if (i === m) {
-      assignMap(nums2[j++])
-    } else if (j === n) {
-      assignMap(nums1[i++])
-    } else if (nums1[i] < nums2[j]) {
-      assignMap(nums1[i++])
-    } else {
-      assignMap(nums2[j++])
-    }
-    function assignMap (arg) {
-      map[i + j - 1] = arg
-    }
-  }
-  map.forEach((item, index) => nums1[index] = item)
-};
+	let i = m - 1,
+		j = n - 1
+	let len = m + n - 1
+	while (len >= 0) {
+		if (i < 0) {
+			nums1[len--] = nums2[j--]
+		} else if (j < 0) {
+			nums1[len--] = nums1[i--]
+		} else {
+			nums1[len--] = nums1[i] > nums2[j] ? nums1[i--] : nums2[j--]
+		}
+	}
+}
